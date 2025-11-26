79d38bd4fbce5a8d95cbe624668a61a2ab162860
Iago Rivadulla <iagorivadullaact@gmail.com>
2025-11-27 00:58:53 +0100

    Change model filename to model_low.h5

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ from huggingface_hub import hf_hub_download
 # Descargar modelo
 model_path = hf_hub_download(
     repo_id="jamirc/cat_dog_classifier",
-    filename="model.h5")
+    filename="model_low.h5")
 
 app = Flask(__name__)
 NAMES = ['Cat', 'Dog']  # 0 = Cat, 1 = Dog
